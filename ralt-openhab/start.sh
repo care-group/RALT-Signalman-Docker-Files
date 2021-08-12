ec7bbbeaf5b34d6bfc9902bf8a9ca3673a44bad0
Ronnie Smith <ronniesmith@outlook.com>
2021-08-12 17:10:53 +0100

    OpenHAB off host network

--- ralt-openhab/start.sh
+++ ralt-openhab/start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 echo Y | /usr/share/openhab/runtime/bin/restore /home/root/Software/RALT-OpenHAB-Backup/openhab-backup-21_08_05-10_46_53.zip &
-echo badrobot0! | sudo systemctl stop openhab2.service
-sudo rm -rf /var/lib/openhab2/cache/*
-sudo rm -rf /var/lib/openhab2/tmp/*
-sudo systemctl start openhab2.service
+echo badrobot0! | sudo openhab-cli start
