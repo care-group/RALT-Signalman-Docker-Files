b2b2b5c30bd52830642d65f1772b8073500857aa
Ronnie Smith <ronniesmith@outlook.com>
2021-08-12 17:15:30 +0100

    Update start.sh

--- ralt-openhab/start.sh
+++ ralt-openhab/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo Y | /usr/share/openhab/runtime/bin/restore /home/root/Software/RALT-OpenHAB-Backup/openhab-backup-21_08_05-10_46_53.zip &
-echo badrobot0! | sudo openhab-cli start
+openhab-cli start
