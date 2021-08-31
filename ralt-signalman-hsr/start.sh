918c7453dfe49a6786a3a2b3339a09a14c664b84
Ronnie Smith <ronniesmith@outlook.com>
2021-08-31 11:18:21 +0100

    Fixed bad ref in signalman HSR dockerfiles

--- ralt-signalman-hsr/start.sh
+++ ralt-signalman-hsr/start.sh
@@ -6,4 +6,4 @@ export ROS_IP=192.168.1.130
 . /opt/ros/melodic/setup.sh && cd /home/root/catkin_ws && catkin_make
 source /home/root/catkin_ws/devel/setup.bash
 sleep 2
-rosrun ralt_signalman main.py
+rosrun ralt_signalman_hsr main.py
