529721f7ff085558d7be03b3efeba2339df6a993
Ronnie Smith <ronniesmith@outlook.com>
2021-08-31 10:49:24 +0100

    Working version from WS1

--- ralt-semantic-event-logger/start.sh
+++ ralt-semantic-event-logger/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-export ROS_MASTER_URI=http://$(hostname --ip-address):11311
-export ROS_HOSTNAME=$(hostname --ip-address)
+# export ROS_MASTER_URI=http://$(hostname --ip-address):11311
+echo "192.168.1.9 tiago-23c" >> /etc/hosts
+export ROS_MASTER_URI=http://192.168.1.9:11311
+export ROS_IP=192.168.1.130
 . /opt/ros/melodic/setup.sh && cd /home/root/catkin_ws && catkin_make
 source /home/root/catkin_ws/devel/setup.bash
 sleep 2
--- ralt-signalman/start.sh
+++ ralt-signalman/start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-export ROS_MASTER_URI=http://$(hostname --ip-address):11311
-export ROS_HOSTNAME=$(hostname --ip-address)
+# export ROS_MASTER_URI=http://$(hostname --ip-address):11311
+echo "192.168.1.9 tiago-23c" >> /etc/hosts
+export ROS_MASTER_URI=http://192.168.1.9:11311
+export ROS_IP=192.168.1.130
 . /opt/ros/melodic/setup.sh && cd /home/root/catkin_ws && catkin_make
 source /home/root/catkin_ws/devel/setup.bash
-roscore &
 sleep 2
 rosrun ralt_signalman main.py
