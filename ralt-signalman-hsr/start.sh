438f4e068533bf7015ed3cb6351b21d3c5f82715
Ronnie Smith <ronniesmith@outlook.com>
2021-09-01 14:38:45 +0100

    Updated sensors.yaml

--- ralt-signalman-hsr/start.sh
+++ ralt-signalman-hsr/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # export ROS_MASTER_URI=http://$(hostname --ip-address):11311
-echo "192.168.1.9 tiago-23c" >> /etc/hosts
-export ROS_MASTER_URI=http://192.168.1.9:11311
+echo "192.168.1.211 hsrc" >> /etc/hosts
+export ROS_MASTER_URI=http://192.168.1.211:11311/
 export ROS_IP=192.168.1.130
 . /opt/ros/melodic/setup.sh && cd /home/root/catkin_ws && catkin_make
 source /home/root/catkin_ws/devel/setup.bash
