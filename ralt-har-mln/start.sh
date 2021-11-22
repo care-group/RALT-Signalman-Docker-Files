ce391cb22874e3476deb61e719ec874e16b2b354
Ronnie Smith <ronniesmith@outlook.com>
2021-11-22 16:53:42 +0000

    Update start.sh

--- ralt-har-mln/start.sh
+++ ralt-har-mln/start.sh
@@ -6,4 +6,4 @@ export ROS_IP=192.168.1.130
 . /opt/ros/melodic/setup.sh && cd /home/root/catkin_ws && catkin_make
 source /home/root/catkin_ws/devel/setup.bash
 sleep 2
-rosrun ralt_har_mln_basic main.py
+rosrun ralt_har_mln main.py
