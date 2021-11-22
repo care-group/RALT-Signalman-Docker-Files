b5ce8ec492f0a64294e9a176c29ac15eccdfaeb8
Ronnie Smith <ronniesmith@outlook.com>
2021-11-22 16:54:02 +0000

    Update start.sh

--- ralt-apple-watch-publisher/start.sh
+++ ralt-apple-watch-publisher/start.sh
@@ -6,4 +6,4 @@ export ROS_IP=192.168.1.130
 . /opt/ros/melodic/setup.sh && cd /home/root/catkin_ws && catkin_make
 source /home/root/catkin_ws/devel/setup.bash
 sleep 10
-rosrun apple_watch_publisher apple_watch_publisher.py
+rosrun ralt_apple_watch_publisher apple_watch_publisher.py
