926f02892eec1acea30d4fff081776ad16a17228
Ronnie Smith <ronniesmith@outlook.com>
2021-11-25 12:43:43 +0000

    ROS environment variables are now loaded from .env file

--- ralt-signalman-hsr/start.sh
+++ ralt-signalman-hsr/start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # export ROS_MASTER_URI=http://$(hostname --ip-address):11311
-echo "192.168.1.211 hsrc" >> /etc/hosts
-export ROS_MASTER_URI=http://192.168.1.211:11311/
-export ROS_IP=192.168.1.130
+echo $ROS_MASTER_IP $ROS_MASTER_HOSTNAME >> /etc/hosts
 . /opt/ros/melodic/setup.sh && cd /home/root/catkin_ws && catkin_make
 source /home/root/catkin_ws/devel/setup.bash
 sleep 2
