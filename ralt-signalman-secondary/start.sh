c48990fe80775a7efc3ad9cc87be2b1589f78fa5
Ronnie Smith <ronniesmith@outlook.com>
2022-06-29 15:45:26 +0100

    HSR -> secondary

---
+++ ralt-signalman-secondary/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# export ROS_MASTER_URI=http://$(hostname --ip-address):11311
+echo $ROS_MASTER_IP $ROS_MASTER_HOSTNAME >> /etc/hosts
+. /opt/ros/melodic/setup.sh && cd /home/root/catkin_ws && catkin_make
+source /home/root/catkin_ws/devel/setup.bash
+sleep 2
+rosrun ralt_signalman_hsr main.py
